d363265500bda7a7fdb5bb8b07fc6b56f52d8363
JSalicram <JSalicram@users.github.com>
2019-02-26 15:27:48 +0000

    Switches done
    finished level to switch between tilemaps. now adding timer function

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The LevelManager from the Week 6 Lecture
+ */
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    public GameObject tileMapA;
+    public GameObject tileMapB;
+
+    private void Awake()
+    {
+        // set the instance property/variable to this object
+        instance = this;
+
+        if (tileMapA == null || tileMapB == null)
+        {
+
+        }
+    }
+
+
+    
+}
---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        LevelManager.instance.tileMapA.SetActive(!LevelManager.instance.tileMapA.activeSelf);
+        LevelManager.instance.tileMapB.SetActive(!LevelManager.instance.tileMapB.activeSelf);
+    }
+}
